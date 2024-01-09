4666cb4b3535f499b76d3c9275519fb7de276fff
Karan <karannataraj301@gmail.com>
2024-01-09 07:49:56 +0530

    styles updated

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
-
-// const tl = gsap.timeline({
-//     default: {
-//         duration: 1.8,
-//     }
-// })
-
-// tl.set(".home-container1", {
-//     x: -700,
-// })
-//     .set(".name", {
-//         opacity: 0
-//     })
-//     .set(".job_role", {
-//         opacity: 0
-//     })
-//     .set(".home-container2", {
-//         x: -700,
-//     })
-//     .to(".home-container2", {
-//         x: 0,
-//     })
-//     .to(".home-container1", {
-//         x: 0,
-//     })
-//     .to(".name", {
-//         opacity: 1,
-//         duration: 1.5
-//     })
-//     .to(".job_role", {
-//         opacity: 1,
-//         duration: 1.5
-//     })
-
-
 const navbar = document.querySelector(".navbar")
 const icon = document.querySelector(".icon")
 
 
-icon.addEventListener("click",() => {  
+icon.addEventListener("click", () => {
     navbar.classList.toggle("active")
 });
 
 
+window.addEventListener('scroll', () => {
+
+    const position = window.scrollY
+
+    if (position > 10) {
+        navbar.style.backgroundColor = 'rgba(0,0,0,0.7)';
+    } else{
+        navbar.style.backgroundColor = "transparent"
+    }
+
+
+    console.log(position);
+});
